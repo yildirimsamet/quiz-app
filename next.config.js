419f257bdc34ed5f298231bb4dd1a35202184892
Samet Yıldırım <samety3503@gmail.com>
2021-03-28 16:31:50 +0300

    responsive css added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    // API: "https://quiz-app-yildirimsamet.vercel.app",
-    API: "http://localhost:3000",
+    API: "https://quiz-app-yildirimsamet.vercel.app",
+    // API: "http://localhost:3000",
   },
 };
